1809e10885b640c4263bc184cb44a27b19b1c48c
Zayed Ramadan R. <zayedrmdn@gmail.com>
2024-05-02 12:17:48 +0800

    Added Win text

--- turtle_animation.py
+++ turtle_animation.py
@@ -22,11 +22,13 @@ for j in range(15):
 
 plyr1 = Turtle()
 plyr1.color('magenta')
+plyr1.shape('turtle')
+
+plyr1.penup()
 plyr1.goto(-160, 100)
 plyr1.pendown()
 
-for turn in range(10):
-    plyr1.right(36)
+
 
 plyr2 = Turtle()
 plyr2.color('red')
@@ -44,8 +46,7 @@ plyr3.penup()
 plyr3.goto(-160, 40)
 plyr3.pendown()
 
-for turn in range(60):
-    plyr3.right(6)
+
 
 plyr4 = Turtle()
 plyr4.shape('turtle')
@@ -55,11 +56,47 @@ plyr4.penup()
 plyr4.goto(-160, 10)
 plyr4.pendown()
 
-for turn in range(30):
-    plyr4.left(12)
+
+
+randomInt1 = randint(1, 5)
+randomInt2 = randint(1, 5)
+randomInt3 = randint(1, 5)
+randomInt4 = randint(1, 5)
+
+distance1 = 0;
+distance2 = 0;
+distance3 = 0;
+distance4 = 0;
+
+for i in range(100):
+    distance1 += randint(1, 5)
+    distance2 += randint(1, 5)
+    distance3 += randint(1, 5)
+    distance4 += randint(1, 5)
 
 for turn in range(100):
-    plyr1.forward(randint(1, 5))
-    plyr2.forward(randint(1, 5))
-    plyr3.forward(randint(1, 5))
-    plyr4.forward(randint(1, 5))
+    plyr1.forward(randomInt1)
+    plyr2.forward(randomInt2)
+    plyr3.forward(randomInt3)
+    plyr4.forward(randomInt4)
+
+minimum_distance = max(distance1, distance2, distance3, distance4)
+
+if distance1 == minimum_distance:
+    winner = "Player 1 (Magenta) Wins !!!"
+elif distance2 == minimum_distance:
+    winner = "Player 2 (Red) Wins !!!"
+elif distance3 == minimum_distance:
+    winner = "Player 3 (Light Green) Wins !!!"
+else:
+    winner = "Player 4 (Blue) Wins !!!"
+
+plyr1.clear
+plyr2.clear
+plyr3.clear
+plyr4.clear
+
+plyr5 = Turtle()
+plyr5.write(winner, True, align="center", font=("Arial", 40, "normal"))
+
+input("Hit enter.")
